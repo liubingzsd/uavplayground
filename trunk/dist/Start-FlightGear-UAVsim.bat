930483a851e17a834ebcf1c308d8b0d3981742d9
jarontec <jarontec@9030949c-eebd-11dd-b665-5960d87910ab>
2009-02-21 22:45:11 +0000

    Version 1.1

--- trunk/dist/Start-FlightGear-UAVsim.bat
+++ trunk/dist/Start-FlightGear-UAVsim.bat
@@ -1,5 +1,6 @@
 "%PROGRAMFILES%\FlightGear\bin\Win32\fgfs" ^
  "--fg-root=%PROGRAMFILES%\FlightGear\data" ^
+ "--nmea=socket,out,0.5,127.0.0.1,5557,tcp" ^
  "--generic=socket,in,10,127.0.0.1,5556,tcp,UAVsim-Protocol" ^
  "--generic=socket,out,3,127.0.0.1,5555,tcp,UAVsim-Protocol" ^
  "--timeofday=noon"
